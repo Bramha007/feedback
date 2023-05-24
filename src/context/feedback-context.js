d1bd45d6af63784e50e5ae2f92c6612369c918c3
Nitish Choudhary <nitishchy007@gmail.com>
2023-05-24 10:02:41 +0530

    server change

--- src/context/feedback-context.js
+++ src/context/feedback-context.js
@@ -12,7 +12,9 @@ export const FeedbackProvider = ({ children }) => {
 
     const fetchFeedback = async () => {
         setIsLoading(true);
-        const response = await fetch("http://localhost:5000/feedbacks");
+        const response = await fetch(
+            "https://mauve-donkey-belt.cyclic.app/feedbacks"
+        );
         const responseData = await response.json();
         setFeedbacks(responseData);
         setIsLoading(false);
@@ -25,9 +27,12 @@ export const FeedbackProvider = ({ children }) => {
     //Delete feedback
     const deleteFeedback = async (id) => {
         if (window.confirm("Are you sure you want to delete?")) {
-            await fetch(`http://localhost:5000/feedbacks/${id}`, {
-                method: "DELETE",
-            });
+            await fetch(
+                `https://mauve-donkey-belt.cyclic.app/feedbacks/${id}`,
+                {
+                    method: "DELETE",
+                }
+            );
             // const responseData = await response.json();
             setFeedbacks((prev) =>
                 prev.filter((feedback) => feedback.id !== id)
@@ -36,11 +41,14 @@ export const FeedbackProvider = ({ children }) => {
     };
 
     const addFeedback = async (newFeedback) => {
-        const response = await fetch("http://localhost:5000/feedbacks", {
-            method: "POST",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify(newFeedback),
-        });
+        const response = await fetch(
+            "https://mauve-donkey-belt.cyclic.app/feedbacks",
+            {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify(newFeedback),
+            }
+        );
         const responseData = await response.json();
         setFeedbacks((prev) => [{ ...responseData }, ...prev]);
     };
@@ -51,11 +59,14 @@ export const FeedbackProvider = ({ children }) => {
     };
 
     const updateFeedback = async (id, updItem) => {
-        const response = await fetch(`http://localhost:5000/feedbacks/${id}`, {
-            method: "PUT",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify(updItem),
-        });
+        const response = await fetch(
+            `https://mauve-donkey-belt.cyclic.app/feedbacks/${id}`,
+            {
+                method: "PUT",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify(updItem),
+            }
+        );
         const resData = await response.json();
         setFeedbacks((prev) =>
             prev.map((feedback) =>
